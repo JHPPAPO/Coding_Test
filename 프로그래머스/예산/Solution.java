ded56625afd4add8b64ad9d31f27e4f87992778c
JHPPAPO <83057286+JHPPAPO@users.noreply.github.com>
2022-06-21 13:29:19 +0900

    thinking logic commit

---
+++ 프로그래머스/예산/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] d, int budget) {
+        //d는 부서별로 신청한 금액이 들어있는 배열 (각 원소는 부서별로 신청한 금액을 나타냄)
+        //배열의 각 요소를 더해서 최대개수의 요소가 되게하면 되겠네..
+        int answer = 0;
+        int maxCount = 0;
+        int len = d.length;
+        
+        
+        
+        
+        
+    
+        
+        return answer;
+    }
+    
+    //for문 길이 조건에 변수를 다르게 두어서 더하게 한다.(지하철타고가면서 생각)
+}
