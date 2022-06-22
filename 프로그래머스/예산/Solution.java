d6c25bcb7ba3d4d92069e4b8c4e2e48feafd1c57
JHPPAPO <83057286+JHPPAPO@users.noreply.github.com>
2022-06-22 21:58:49 +0900

    Update Solution.java

--- 프로그래머스/예산/Solution.java
+++ 프로그래머스/예산/Solution.java
@@ -1,19 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] d, int budget) {
-        //d는 부서별로 신청한 금액이 들어있는 배열 (각 원소는 부서별로 신청한 금액을 나타냄)
-        //배열의 각 요소를 더해서 최대개수의 요소가 되게하면 되겠네..
         int answer = 0;
-        int maxCount = 0;
+        
+        //배열을 정렬시킨다.
+        Arrays.sort(d);
+        
         int len = d.length;
         
+        int sum = 0;
         
-        
-        
-        
-    
+        for(int i = 0; i < len; i++) {
+            sum += d[i];
+            if (sum <= budget) {
+                answer++;    
+            } else {
+                break;       
+            }
+        }
         
         return answer;
     }
-    
-    //for문 길이 조건에 변수를 다르게 두어서 더하게 한다.(지하철타고가면서 생각)
 }
