eba34a54ee0f6dcf724c288bf8fe432faf3f941c
JHPPAPO <83057286+JHPPAPO@users.noreply.github.com>
2022-07-12 08:57:26 +0900

    Create Main.java

---
+++ Baekjoon/2753/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        int answer = 0;
+        if(year % 4 == 0) {
+            if(year % 100 != 0 || year % 400 == 0) {
+                answer = 1;
+            }
+        }
+        System.out.print(answer);
+        scanner.close();
+    }
+}
