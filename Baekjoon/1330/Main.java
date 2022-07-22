080159938625159fceb55ef636adad6b32135cc9
radishkiller <83057286+radishkiller@users.noreply.github.com>
2022-07-22 19:18:43 +0900

    Create Main.java

---
+++ Baekjoon/1330/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        String answer;
+        if (A > B) {
+            answer = ">";
+        } else if (A < B) {
+            answer = "<";
+        } else {
+            answer = "==";
+        }
+        System.out.print(answer);
+    }
+}
